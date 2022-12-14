d070f127c73a28b1321a4cc438c6017c6e90c10f
Denor74 <den74@inbox.ru>
2022-12-14 14:10:28 +0500

    task 4 new

--- task4/script.js
+++ task4/script.js
@@ -1,11 +1,13 @@
+// получаем в константу данные из тэга a
 const promptAHref = document.querySelector('a');
 
+//определяем функцию с действием по событию click по содержимому тега а
 promptAHref.addEventListener("click", () => {
-  const userText = prompt("Изменение текста ссылки");
-  // Вывод в консоль значение введённое в окне prompt - для проверки
+  const userText = prompt("Изменение текста ссылки", "Введите новый текст ссылки");
+  // Вывод в консоль значение введённое в окне prompt - для проверки (сделал для себя, можно не писать)
   console.log(userText);
   // забираем значение из тэга a
   const userTextField = document.querySelector('a');
-  // меняем 
+  // меняем текст ссылки на введёный пользователем
   userTextField.textContent = userText
 });
